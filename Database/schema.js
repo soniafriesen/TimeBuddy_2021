48c3b6c6d1622420b9b53a53d70aae22e505325b
soniafriesen <58013701+soniafriesen@users.noreply.github.com>
2021-10-03 14:46:16 -0400

    Update schema.js

--- Database/schema.js
+++ Database/schema.js
@@ -16,6 +16,23 @@ type Query {
  getallemployeesbymanager(managerid:Int): [Employee]
  getspecificemployee(empid:Int): Employee
  deleteemployee(empid:Int): String
+
+ """ Shift query """
+ getallshifts(compname:String):[Shift]
+ getspecificshift(shiftid:Int): Shift
+ deleteshift(shiftid:Int): String
+
+ """ Shift Pool query """
+ getallinpool(compname:String):[Shiftpool]
+ getspecificpoolshift(shiftid:Int):Shiftpool
+ removeshiftfrompool(shiftid:Int, empid:Int):String
+
+
+ """ Meeting query """
+ getallmeetings(compname:String):[Meeting]
+ showspecificmeeting(meetingid:Int):Meeting
+ removemeeting(meetingid:Int):String
+
 }
 
 """ --Company Type--  """
@@ -53,6 +70,35 @@ dob: String,
 startdate: String,
 }
 
+"""  --Shifts Type--   """
+type Shift{
+    compname:String,
+    shiftid:Int,
+    empid:Int,
+    date:String,
+    starttime:String,
+    endtime:String,
+}
+
+"""  --Shift Pool Type--   """
+type Shiftpool{
+    compname:String,
+    shiftid:Int,
+    date:String,
+    starttime:String,
+    endtime:String,
+}
+
+"""  --Meetings Type--   """
+type Meeting{
+    compname:String,
+    meetingid:Int,
+    empid:Int,
+    date:String,
+    starttime:String,
+    message:String,
+}
+
 """ --Mutations Types--"""
 type Mutation{ 
 
@@ -66,10 +112,23 @@ type Mutation{
     updatemanagerdepartment(empid: Int, department:String): Manager      
 
     """Employee Mutations"""
-    addemployee(compname:String, managerid:Int, department:String, firstname: String, lastname: String, email:String, dob: String): String
+    addemployee(compname:String, managerid:Int, department:String, firstname: String, lastname: String, email:String, dob: String): Employee
     updateemployeeemail(empid: Int,email: String): Employee
     updateemployeedepartment(empid: Int, department:String): Employee 
 
+    """Shift Mutations"""
+    addshift(compname:String,empid:Int, date:String, starttime:String, endtime:String): Shift
+    updateshiftstarttime(shiftid:Int,starttime:String): Shift
+    updateshiftendtime(shiftid:Int,endtime:String): Shift
+    switchshift(shiftid:Int,empid:Int): Shift
+
+    """Shift Pool Type"""
+    addshifttopool(compname:String,shiftid:Int): Shiftpool
+
+    """Meeting Type"""
+    postameeting(compname:String, empid:Int,date:String,starttime:String,message:String): Meeting
+    changemeetingtime(meetingid:Int, starttime:String):Meeting
+    changemeetingdate(meetingid:Int, date:String):Meeting
 }
 `);
 module.exports = { schema };

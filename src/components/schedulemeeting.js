9dbf7baf4a0f79bab2ac0ef59b939550044b7db8
yulet <yulet@users.noreply.github.com>
2021-10-10 15:34:47 -0400

    added more fields to schedule meeting

--- src/components/schedulemeeting.js
+++ src/components/schedulemeeting.js
@@ -1,15 +1,27 @@
 import React from "react";
 import {TextField, Button, Typography, CardContent } from '@material-ui/core'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated';
 
 export default function ScheduleMeeting() {
+    const options = [
+        { value: 'employee1', label: 'employee1' },
+        { value: 'employee2', label: 'employee2' },
+        { value: 'employee3', label: 'employee3' },
+        { value: 'employee4', label: 'employee4' }
+      ]
+      const animatedComponents = makeAnimated();
     
         return (
             <CardContent>
                 <Typography variant="h4"  style={{marginTop:'20px'}} color="primary">Schedule Meeting</Typography>
 
                 <form style={{width:'85%', marginLeft:'5%', alignItems:'center', justifyContent: "center"}} align="center">
+
 <div>
 
+
+<TextField fullWidth label="Title" variant="standard" margin="dense" size="small" required />
 
 <Typography style={{marginTop:'10px'}}>Date
                 <input style={{margin:'10px', width:'120px'}} fullWidth label="Date" variant="standard" type="date" margin="large" required />
@@ -17,8 +29,25 @@ export default function ScheduleMeeting() {
                 <Typography style={{marginTop:'10px'}}>Time
   <input style={{margin:'10px', width:'120px'}} fullWidth label="Time" type="time" variant="standard"  margin="dense" size="large" required />
   </Typography>
-  <TextField fullWidth label="Description" variant="standard" margin="dense" size="small" required />
+
+  <TextField fullWidth label="Estimated Length in Minutes" variant="standard" margin="dense" size="small" required />
+
+  <TextField fullWidth label="Location" variant="standard" margin="dense" size="small" required />
+
+  <TextField fullWidth label="Notes" variant="standard" margin="dense" size="small" />
+
+
   </div>
+
+<Typography color="primary" style={{marginTop:'20px', marginBottom:'10px'}}>Employees</Typography>
+
+  <Select
+      closeMenuOnSelect={false}
+      components={animatedComponents}
+      defaultValue={[options[0], options[1]]}
+      isMulti
+      options={options}
+    />
 
       <div>
   <Button style={{marginTop:'20px'}} type="submit" variant="contained" color="primary">

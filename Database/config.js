81344841e13f3e001f99f55b8a23e368df736081
soniafriesen <58013701+soniafriesen@users.noreply.github.com>
2021-10-14 20:10:27 -0400

    Update config.js

--- Database/config.js
+++ Database/config.js
@@ -2,11 +2,14 @@ const dotenv = require("dotenv");
 dotenv.config();
 module.exports = {
   companies: process.env.COMPANYCOLLECTION,
-  managers: process.env.MANAGERCOLLECTION,
   employees: process.env.EMPLOYEECOLLECTION,
   shifts: process.env.SHIFTCOLLECTION,
   pool: process.env.SHIFTPOOLCOLLECTION,
   meetings: process.env.MEETINGCOLLECTION,
+  payrolls: process.env.PAYROLLCOLLECTION,
+  timesoff:process.env.TIMEOFFCOLLECTION,
+  emergencies:process.env.EMERGENCYCOLLECTION,
+  signins:process.env.SIGNINCOLLECTION,
   atlas: process.env.DBURL,
   appdb: process.env.DB,
   port: process.env.PORT,
